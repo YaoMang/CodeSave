8abeaa739157bd3b5c5d9357c48d1faedaa33cff
YaoMang <YaoMangYM@gmail.com>
2024-07-20 22:30:24 +0800

    update fixed

--- Cpp Primer/Chapter8/8.5.cpp
+++ Cpp Primer/Chapter8/8.5.cpp
@@ -9,7 +9,7 @@ int read(string& filename, vector<string>& store)
     ifstream input(filename);
     if(input){
         string buff;
-        while(getline(input, buff))
+        while(input >> buff)
             store.push_back(buff);
     }
     return 0;
