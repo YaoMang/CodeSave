ed22371204ff2ce473a05c29e0d2af2ea0e29827
YaoMang <YaoMangYM@gmail.com>
2024-07-20 21:48:48 +0800

    8.2 added

---
+++ Cpp Primer/Chapter8/8.2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+istream& readAndPrint(istream& is)
+{
+    char in;
+    while (!is.eof())
+    {
+        is >> in;
+        cout << in;
+    }
+    
+    is.clear();
+
+    return is;
+}
+
+int main()
+{
+    readAndPrint(cin);
+
+    return 0;
+}
