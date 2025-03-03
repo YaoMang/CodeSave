eae399501dd1b0e4b704a50e7ba0eb23c4315eb1
YaoMang <YaoMangYM@gmail.com>
2025-03-03 15:21:44 +0800

    p4 update

--- leetcode/Cpp/4. Median of Two Sorted Arrays.cpp
+++ leetcode/Cpp/4. Median of Two Sorted Arrays.cpp
@@ -1,3 +1,4 @@
+// Brutal Solution
 class Solution {
     public:
         double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
@@ -20,3 +21,47 @@ class Solution {
             }
         }
     };
+
+    // Two Pointer Solution
+    class Solution {
+        public:
+            double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+                // Two Pointers
+        
+                // size of two arrays
+                int sz_1 = nums1.size();
+                int sz_2 = nums2.size();
+                
+                // pointer definiton
+                int pos1 = 0, pos2 = 0, now = 0, prev = 0;
+                for (int cnt = 0; cnt <= (sz_1 + sz_2) / 2; cnt++) {
+                    prev = now;
+                    // BOth of them still has nums
+                    if (pos1 != sz_1 && pos2 != sz_2) {
+                        if (nums1[pos1] <= nums2[pos2]) {
+                            now = nums1[pos1];
+                            pos1++;
+                        }
+                        else {
+                            now = nums2[pos2];
+                            pos2++;
+                        }
+                    }
+                    else if (pos1 < sz_1) { // nums1 left
+                        now = nums1[pos1];
+                        pos1++;
+                    }
+                    else { // nums2 left
+                        now = nums2[pos2];
+                        pos2++;
+                    }
+                }
+        
+                if ((sz_1 + sz_2) % 2 == 1) {
+                    return static_cast<double>(now);
+                }
+                else {
+                    return (static_cast<double>(prev) + static_cast<double>(now)) / 2;
+                }
+            }
+        };
