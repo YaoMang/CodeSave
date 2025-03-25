f66b8fe865d517c7084a00d7afbf6dd456c1566d
YaoMang <YaoMangYM@gmail.com>
2025-03-25 19:14:27 +0800

    leetcode update

--- leetcode/Cpp/1. Two Sum.cpp
+++ leetcode/Cpp/1. Two Sum.cpp
@@ -1,3 +1,5 @@
+// 这个算法是暴力匹配算法，其时间复杂度为O(n^2)
+
 #include <vector>
 
 using namespace std;
@@ -25,3 +27,39 @@ public:
         return vector<int> {pos1, pos2};
     }
 };
+
+// 可以对数组排序，然后使用二分搜索，这时的时间复杂度为O(nlogn)
+
+// 还可以使用哈希表，时间复杂度为O(n)
+class Solution {
+    public:
+        vector<int> twoSum(vector<int>& nums, int target) {
+            unordered_map<int, vector<int>> um;
+            vector<int> pos;
+            // 建立哈希表
+            for(int i = 0; i < nums.size(); i++){
+                um[nums[i]].push_back(i);
+            }
+    
+            // 查找对应值
+            for(const auto& i : nums){
+                auto it = um.find(target - i);
+                if(it != um.end()){ // 找到了对应的vecetor
+                    if(target - i == i){ // 两值相等
+                        if(it->second.size() == 2){ // 确定有两个这样的值是存在的
+                            pos.push_back(it->second.front());
+                            pos.push_back(it->second.back());
+                            break;
+                        }
+                    }
+                    else{ // 两值不等
+                        pos.push_back(it->second.front());
+                        pos.push_back(um[i].front());
+                        break;
+                    }
+                }
+            }
+    
+            return pos;
+        }
+    };
