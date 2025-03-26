b2ecec85a39ee4dba9b4322194fb47166176cd1e
YaoMang <YaoMangYM@gmail.com>
2025-03-26 08:33:02 +0800

    leetcode cn update

---
+++ leetcode/CNCpp/141. 环形链表.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+    public:
+        bool hasCycle(ListNode *head) {
+            ListNode* fast = head;
+            ListNode* slow = head;
+    
+            while(fast != nullptr && slow != nullptr){
+                if(slow){
+                    slow = slow->next;
+                }
+                if(fast){
+                    fast = fast->next;
+                    if(fast){
+                        fast = fast->next;
+                    }
+                    else{
+                        break;
+                    }
+                }
+    
+                if(slow == fast){
+                    return true;
+                }
+            }
+    
+            return false;
+        }
+    };
